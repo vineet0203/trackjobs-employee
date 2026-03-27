d1cd5205d3231aa5561214650cc9d3b319f52e8a
Ajinkya Mhetre <ajinkyamhetre01@gmail.com>
2026-03-27 15:20:35 +0530

    Remove router basename for subdomain deployment

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,9 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
-const routerBasename = import.meta.env.PROD ? '/employee' : '/';
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={routerBasename}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
